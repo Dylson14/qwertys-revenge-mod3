dd20f45044613d5f0b6ab98fb19fe906690803a3
Dylson Oliveira <dylsonuni@gmail.com>
2022-07-19 15:30:42 +0100

    added functionality to combine multiple arrays into one array

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 //     "IMPORTANT: A series of prompts will be presented with password criteria. When prompted, decide whether you wish to include the criteria by clicking 'ok' for yes, or 'cancel' to not include it in your final password."
 //   );
 // };
-
+  
   const combineArr = [];
 
   const numArr = [0, 1, 2, 3];
@@ -17,36 +17,37 @@ var generateBtn = document.querySelector("#generate");
 
   const specialCharArr = ["%", "@", "$", "&"];
 
-const shuffleArrFunc = (combineArr) => {
-
-  console.log("We have entered: " + JSON.stringify(combineArr));
-  // const shuffleArray = combineArr.sort((a, b) => 0.5 - Math.random());
-  // console.log("This is the shuffled Arr: " + shuffleArray);
-
-
-}
+// const shuffleArrFunc = (finalArr) => {
+//   return finalArr;
+// }
 
 const promptFunc = () => {
 
   let lowerCase = confirm("Should your password have lower-case characters?");
   if (lowerCase) {
-    // console.log({ lowerCase });
     console.log("lower-case accepted");
     combineArr.push(lowerArr);
   } else if (!lowerCase) {
-    // console.log({ lowerCase });
     console.log("lower-case rejected");
+  }
+  
+  console.log("*-------------------------------------------*");
+
+  let numeric = confirm("Should your password have numbers?");
+  if (numeric) {
+    console.log("numbers accepted");
+    combineArr.push(numArr);
+  } else if (!numeric) {
+    console.log("numbers rejected");
   }
 
   console.log("*-------------------------------------------*");
 
   let upperCase = confirm("Should your password have upper-case characters?");
   if (upperCase) {
-    // console.log({ upperCase });
     console.log("upper-case accepted");
     combineArr.push(upperArr);
   } else if (!upperCase) {
-    // console.log({ upperCase });
     console.log("upper-case rejected");
   }
 
@@ -54,39 +55,33 @@ const promptFunc = () => {
 
   let specialChar = confirm("Should your password have special characters?");
   if (specialChar) {
-    // console.log({specialChar});
     console.log("special characters accepted");
     combineArr.push(specialCharArr);
   } else if (!specialChar) {
-    // console.log({specialChar});
     console.log("special characters rejected");
   }
 
   console.log("*-------------------------------------------*");
-
-  let numeric = confirm("Should your password have numbers?");
-  if (numeric) {
-    // console.log({numeric});
-    console.log("numbers accepted");
-    combineArr.push(numArr);
-  } else if (!numeric) {
-    // console.log({numeric});
-    console.log("numbers rejected");
-  }
-
-  console.log("This is the final arr: " + combineArr); 
   
-  shuffleArrFunc(combineArr);
+  // console.log("This is the final arr: " + JSON.stringify({combineArr})); 
+  var finalArr = [].concat.apply([], combineArr);
+  console.log(finalArr);
+
+  return finalArr
+
 };
 
 function generatePassword() {
   promptFunc();
+  // shuffleArrFunc(finalArr);
+ 
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  console.log("Ah shizzle here we go again: " + JSON.stringify({password}));
 
   passwordText.value = password;
 }
